5085fa2a21088ed9b3457fe2013b1e975a2aabdc
pavel-else <de.pavel.se@yandex.ru>
2017-08-20 23:35:33 +0300

    layout block-7 telephone soc-seti

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,12 @@ var minifyCss 	= require('gulp-minify-css');
 var sass 		= require('gulp-sass');
 var notify 		= require('gulp-notify');
 
-gulp.task('mincss', function() { 
- 	return gulp.src('sass/main.scss')
-   	.pipe(sass().on('error', sass.logError))
-   	.pipe(minifyCss())
-   	.pipe(gulp.dest('css'))
-    .pipe(notify('Done! master zooboole.'));
-});
+gulp.task('mincss', function(){
+  return gulp.src('sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('css'))
+ });
 
 
 gulp.task('watcher', function () {
